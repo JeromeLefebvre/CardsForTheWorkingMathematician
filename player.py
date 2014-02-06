c78f6bc298e3f1aa204a4270fb77726a92a7583e
marioga <mgarcia.math@gmail.com>
2014-02-06 13:07:19 -0800

    More changes to the class Player
    Must update class Hand to be able to discard externally. This is needed
    for splitting. Maybe it would be more convenient to create a split
    method in hand that checks that one has a pair (valid splitting), leaves
    a card in the hand and returns a new hand containing the other card.

--- player.py
+++ player.py
@@ -55,13 +55,33 @@ class NormalPlayer(Player):
     def __init__(self, hand=None, money=0, name = "Stranger"):
         Player.__init__(self, hand, money, name)
 
-    def updateAfterBet(self,n):
+    def hasEnoughToBet(self,bet):
+        '''Check whether a player has enough to bet'''
+        return self.money()>=bet
+
+    def updateAfterDouble(self,card,bet):
+        '''Updates player's instance after doubling and makes sure player has enough'''
+        try:
+            if self.hasEnoughToBet(int(bet)):
+                self._money-=bet
+                self._hand.receive(card)
+            else:
+                print("Not enough money to double")
+        except ValueError:
+            print ("Bet is not an integer")
+
+    def updateAfterSplit(self,bet):
+        '''Updates player after he split his pair. Creates an alternative hand'''
+        pass #TBW Must update the class hand to be able to discard.
+
+
+    def updateAfterBet(self,bet):
         '''Updates player's money after betting and makes sure player has enough'''
         try:
-            if self._money<int(n):
-                print("Not enough money")
+            if self.hasEnoughToBet(int(bet)):
+                self.money-=bet
             else:
-                self._money-=n
+                print("Not enough money to bet")
         except ValueError:
             print ("Bet is not an integer")
 
@@ -73,10 +93,17 @@ class Dealer(Player):
         Player.__init__(self, hand, 0, "Dealer")
 
 if __name__ == '__main__':
-    myplayer=NormalPlayer(Hand([Card(),Card()]))
+    myplayer=NormalPlayer(Hand([Card(),Card()]),5)
     myplayer.displayHand()
     print (myplayer.hand().value())
     myplayer.updateAfterHit(Card())
     myplayer.displayHand()
     print (myplayer.hand().value())
+    myplayer.updateAfterDouble(Card(),5)
+    myplayer.displayHand()
+    print (myplayer.hand().value())
+    myplayer.updateAfterDouble(Card(),5)
+    myplayer.displayHand()
+    print (myplayer.hand().value())
+
 
