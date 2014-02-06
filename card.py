d711351a422b4f8a91d56bccbcee588d815e35ac
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-02-05 23:22:10 -0800

    Fixed the formatting for the card

--- card.py
+++ card.py
@@ -1,5 +1,5 @@
 
-class card(object):
+class Card(object):
 	''' The card object is an object that holds a rank and a suit
 	the suitd is sorted as:
 	clubs = 0, diamonds = 1, hearts = 2, spades = 3
@@ -7,13 +7,14 @@ class card(object):
 	ace = 0, 2 = 1, ... , 10 = 9, Jack = 10, Queen = 11, King = 12
 	'''
 	# Here are class variables
-	_suits = {3:'Clubs', 2:'Diamonds', 1:'Hearts', 0:'Spades'}
+	_SUITS = {3:'Clubs', 2:'Diamonds', 1:'Hearts', 0:'Spades'}
 	# Since values are unique, it is nice to look them the other way around
-	_suitsReverse = {'Spades': 0, 'Clubs': 3, 'Diamonds': 2, 'Hearts': 1, 'S':0, 'C': 3, 'D':2, 'H':1}
-	_ranks = {0: 'Ace', 1: '2', 2: '3', 3: '4', 4: '5',5: '6',6: '7', 7: '8', 8: '9', 9:'10', 10:'Jack', 11:'Queen', 12:'King'}
-	_ranksReverse = {'Ace': 0, '10': 9, 'Jack': 10, 'King': 12, '6': 5, '7': 6, '4': 3, '5': 4, '2': 1, '3': 2, 'Queen': 11, '8': 7, '9': 8, 'A':0, 'Q':11, 'J': 10, 'K': 12}
+	_SUITS_REVERSE = {'Spades': 0, 'Clubs': 3, 'Diamonds': 2, 'Hearts': 1, 'S':0, 'C': 3, 'D':2, 'H':1}
+	_RANKS = {0: 'Ace', 1: '2', 2: '3', 3: '4', 4: '5',5: '6',6: '7', 7: '8', 8: '9', 9:'10', 10:'Jack', 11:'Queen', 12:'King'}
+	_RANKS_REVERSE = {'Ace': 0, '10': 9, 'Jack': 10, 'King': 12, '6': 5, '7': 6, '4': 3, '5': 4, '2': 1, '3': 2, 'Queen': 11, '8': 7, '9': 8, 'A':0, 'Q':11, 'J': 10, 'K': 12}
+
 	# The value of each suits
-	_blackjackValue = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'Jacks':10,'Queen':10,'King':10,'Ace':1}
+	_BLACKJACK_VALUE = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'Jacks':10,'Queen':10,'King':10,'Ace':1}
 	def __init__(self,rank=None,suit=None):
 		# if you don't specify a rank, pick one at random
 		if rank == None:
@@ -25,8 +26,8 @@ class card(object):
 				assert( 0 <= rank <= 12)
 				self._rank = rank
 			elif isinstance(rank, str):
-				assert( rank in [ key for key in card._ranksReverse ] )
-				self._rank = card._ranksReverse[rank]
+				assert( rank in [ key for key in Card._RANKS_REVERSE ] )
+				self._rank = Card._RANKS_REVERSE[rank]
 			else:
 				# Gave a weird input
 				raise ValueError
@@ -39,8 +40,8 @@ class card(object):
 				assert( 0 <= suit <= 3)
 				self._suit = suit
 			elif isinstance(suit, str):
-				assert( suit in [ key for key in card._suitsReverse ] )
-				self._suit = card._suitsReverse[suit]
+				assert( suit in [ key for key in Card._SUITS_REVERSE ] )
+				self._suit = Card._SUITS_REVERSE[suit]
 			else:
 				# Gave a weird input
 				raise ValueError
@@ -79,13 +80,13 @@ class card(object):
 			
 	def __eq__(self, other):
 		# Must compare against a card
-		if not isinstance(other,card):
+		if not isinstance(other,Card):
 			return False
 		# We use withsuits since we want actual equality
 		return self._compare(other, lambda x,y: x == y, withsuits=True)
 
 	def __ne__(self,other):
-		if not isinstance(other,card):
+		if not isinstance(other,Card):
 			return True
 		# != is more complicated, since we want to return True if either the rank or the suit differs
 		withsuits=True
@@ -97,54 +98,54 @@ class card(object):
 		return method(self.rank(), other.rank())
 
 	def __gt__(self,other):
-		if isinstance(other, card):
+		if isinstance(other, Card):
 			return self._compare(other, lambda x,y: x > y)
 		elif isinstance(other, int):
-			return self > card(other)
+			return self > Card(other)
 
 	def __ge__(self,other):
-		if isinstance(other, card):
+		if isinstance(other, Card):
 			return self._compare(other, lambda x,y: x >= y)
 		elif isinstance(other, int):
-			return self >= card(other)
+			return self >= Card(other)
 
 	def __lt__(self,other):
-		if isinstance(other, card):
+		if isinstance(other, Card):
 			return self._compare(other, lambda x,y: x < y)
 		elif isinstance(other, int):
-			return self < card(other)
+			return self < Card(other)
 
 	def __le__(self,other):
-		if isinstance(other, card):
+		if isinstance(other, Card):
 			return self._compare(other, lambda x,y: x <= y)
 		elif isinstance(other, int):
-			return self <= card(other)
+			return self <= Card(other)
 
 	def __str__(self):
 		''' Used with the print function
 		>>> print(card('Q'))
 Queen of Hearts
 		'''
-		return str(self._ranks[self._rank]) + " of " + str(self._suits[self._suit])
+		return str(self._RANKS[self._rank]) + " of " + str(self._SUITS[self._suit])
 
 	def __repr__(self):
 		'''>>> card('Q')
 Queen of Spades
 		'''
-		return str(self._ranks[self._rank]) + " of " + str(self._suits[self._suit])
+		return str(self._RANKS[self._rank]) + " of " + str(self._SUITS[self._suit])
 
 	@classmethod
 	def suits_name(cls,suit):
-		return card._suits[suit]
+		return Card._SUITS[suit]
 
 	@classmethod
 	def ranks_name(cls,rank):
-		return card._ranks[rank]
+		return Card._RANKS[rank]
 
 	def blackjackValue(self,aceWorthElven=False):
-		if aceWorthElven and self.rank() == card._ranksReverse[("Ace")]:
+		if aceWorthElven and self.rank() == Card._RANKS_REVERSE[("Ace")]:
 			return 11
-		return card._blackjackValue[ card._ranks[self.rank()]]
+		return Card._BLACKJACK_VALUE[ Card._RANKS[self.rank()]]
 
 	def isAce(self):
 		return self._rank == 0
@@ -155,65 +156,65 @@ class TestHand(unittest.TestCase):
 		pass
 
 	def test_sort(self):
-		Q = card('Q')
-		J = card('J')
-		t = card('10')
+		Q = Card('Q')
+		J = Card('J')
+		t = Card('10')
 		self.assertEqual( sorted([Q, J, t]) , [t, J, Q])
-		one = card('3', 'H')
-		two = card('4', 'S')
+		one = Card('3', 'H')
+		two = Card('4', 'S')
 		self.assertEqual( sorted([two,one]), [one,two])
-		one = card(1, 'H')
-		two = card(2, 'S')
+		one = Card(1, 'H')
+		two = Card(2, 'S')
 		self.assertEqual( sorted([two,one]), [one,two])		
 		
-		oneH = card('2', 'H')
-		oneS = card('2', 'S')
-		oneC = card('2', 'C')
-		oneD = card('2', 'D')
+		oneH = Card('2', 'H')
+		oneS = Card('2', 'S')
+		oneC = Card('2', 'C')
+		oneD = Card('2', 'D')
 		self.assertEqual( sorted( [oneH, oneS], key= lambda x: (x.rank(), x.suit()) ), [oneS, oneH] )
-		self.assertEqual( sorted( [oneH, oneS, oneC, oneD], key= lambda x: card.cardCompare(x,withsuits=True)), [oneS, oneH, oneD, oneC] )
+		self.assertEqual( sorted( [oneH, oneS, oneC, oneD], key= lambda x: Card.cardCompare(x,withsuits=True)), [oneS, oneH, oneD, oneC] )
 
-		K = card('K')
-		A = card('A')
-		self.assertEqual( sorted( [K, A], key= lambda x: card.cardCompare(x,withsuits=True)), [A,K] )
+		K = Card('K')
+		A = Card('A')
+		self.assertEqual( sorted( [K, A], key= lambda x: Card.cardCompare(x,withsuits=True)), [A,K] )
 		def aceWorthElven(x):
 			if x.rank() == 0:
 				return 11
 			else:
 				return x.rank()
-		two = card('2')
+		two = Card('2')
 		# One compare with Ace = 0, one with Ace = 11
-		self.assertEqual( sorted( [A,two], key= lambda x: card.cardCompare(x,withsuits=True)), [A,two] )
+		self.assertEqual( sorted( [A,two], key= lambda x: Card.cardCompare(x,withsuits=True)), [A,two] )
 		self.assertEqual( sorted( [A,two], key= aceWorthElven), [two,A] )
 
 	def test_compare(self):
-		self.assertTrue( card('K') > card('Q'))
-		self.assertTrue( card('K') > card('J'))
-		self.assertTrue( card('5') < 6)
-		self.assertTrue( card('5') <= 6)
-		self.assertFalse( card('5') >= 6)
+		self.assertTrue( Card('K') > Card('Q'))
+		self.assertTrue( Card('K') > Card('J'))
+		self.assertTrue( Card('5') < 6)
+		self.assertTrue( Card('5') <= 6)
+		self.assertFalse( Card('5') >= 6)
 
 	def test_equality(self):
-		self.assertEqual( card('K','H') , card('K','H'))
-		self.assertTrue( card() != 5 )
-		self.assertTrue( card('K','H') != card('K','S'))
+		self.assertEqual( Card('K','H') , Card('K','H'))
+		self.assertTrue( Card() != 5 )
+		self.assertTrue( Card('K','H') != Card('K','S'))
 
 	def test_creation(self):
-		# Verify that I can't create a card made of Junk
-		self.assertRaises( ValueError, card, [])
+		# Verify that I can't create a Card made of Junk
+		self.assertRaises( ValueError, Card, [])
 
 	def test_choice(self):
 		# verify random creation
-		c = card()
+		c = Card()
 		self.assertTrue(c.suit() in [0,1,2,3])
 
 	def test_classMethod(self):
-		self.assertEqual(card.suits_name(2),'Diamonds')
+		self.assertEqual(Card.suits_name(2),'Diamonds')
 
 	def test_blackjack(self):
-		self.assertEqual( card('Q').blackjackValue(), 10)
-		self.assertEqual( card('A').blackjackValue(), 1)
-		self.assertEqual( card('A').blackjackValue(True), 11)
+		self.assertEqual( Card('Q').blackjackValue(), 10)
+		self.assertEqual( Card('A').blackjackValue(), 1)
+		self.assertEqual( Card('A').blackjackValue(True), 11)
 
 if __name__ == "__main__":
 	unittest.main()
