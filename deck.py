2a9b295c6a4b376b11e0b4c06708f22529cf3789
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-02-05 23:14:40 -0800

    Fix the formatting in Hand and Deck classes

--- deck.py
+++ deck.py
@@ -12,48 +12,46 @@
 ''' This is the implementation of the deck class. A "deck" consists of _size
 decks of 52 cards.
 '''
-from card import card
 from random import shuffle
+import unittest
 
-class deck:
+from card import Card
+
+
+class Deck:
     def __init__(self, size=8):
         try:
-            self._size=int(size)
+            self._size = int(size)
         except ValueError:
             print ("Input for deck is not an integer")
         # We now populate the deck
-        self._cards=[]
+        self._cards = []
         for num_decks in range(self._size):
             for rank in range(13):
                 for suit in range(4):
                     self._cards.append(card(rank,suit))
 
     def pop(self):
+        ''' pop() -> Card -- returns the top of the card '''
         return self._cards.pop()
 
     def cardsLeft(self):
+        ''' cardsLeft() -> int -- returns the number of cards left '''
         return len(self._cards)
 
     def shuffle(self):
+        ''' shuffle() -> None -- shuffles the deck in place '''
         shuffle(self._cards)
 
-import unittest
 class TestHand(unittest.TestCase):
     def setup(self):
         pass
 
     def test_basic(self):
-        self.dealerdeck = deck(2)
-        self.assertEqual(self.dealerdeck.cardsLeft(), 52*2)
-        self.dealerdeck.pop()
-        self.assertEqual(self.dealerdeck.cardsLeft(), 52*2 - 1)
+        self.dealerDeck = Deck(2)
+        self.assertEqual(self.dealerDeck.cardsLeft(), 52*2)
+        self.dealerDeck.pop()
+        self.assertEqual(self.dealerDeck.cardsLeft(), 52*2 - 1)
 
 if __name__ == '__main__':
     unittest.main()
-    d = deck()
-    d.shuffle()
-    print (d.pop())
-    print (d.cardsLeft())
-    print (d.pop())
-    print (d.cardsLeft())
-
