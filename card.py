1d584c2d57a40517d220419fcb9250f2b1d20d86
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-02-05 16:05:45 -0800

    Replace suite by suit

--- card.py
+++ card.py
@@ -1,18 +1,19 @@
+
 class card(object):
-	''' The card object is an object that holds a rank and a suite
-	the suited is sorted as:
+	''' The card object is an object that holds a rank and a suit
+	the suitd is sorted as:
 	clubs = 0, diamonds = 1, hearts = 2, spades = 3
 	the ranks are:
 	ace = 0, 2 = 1, ... , 10 = 9, Jack = 10, Queen = 11, King = 12
 	'''
 	# Here are class variables
-	_suites = {0:'Clubs', 1:'Diamonds', 2:'Hearts',3:'Spades'}
+	_suits = {0:'Clubs', 1:'Diamonds', 2:'Hearts', 3:'Spades'}
 	# Since values are unique, it is nice to look them the other way around
-	_suites_reverse = {'Spades': 3, 'Clubs': 0, 'Diamonds': 1, 'Hearts': 2, 'S':3, 'C': 0, 'D':1, 'H':2}
+	_suits_reverse = {'Spades': 3, 'Clubs': 0, 'Diamonds': 1, 'Hearts': 2, 'S':3, 'C': 0, 'D':1, 'H':2}
 	_ranks = {0: 'Ace', 1: '2', 2: '3', 3: '4', 4: '5',5: '6',6: '7', 7: '8', 8: '9', 9:'10', 10:'Jack', 11:'Queen', 12:'King'}
 	_ranks_reverse = {'Ace': 0, '10': 9, 'Jack': 10, 'King': 12, '6': 5, '7': 6, '4': 3, '5': 4, '2': 1, '3': 2, 'Queen': 11, '8': 7, '9': 8, 'A':0, 'Q':11, 'J': 10, 'K': 12}
 
-	def __init__(self,rank=None,suite=None):
+	def __init__(self,rank=None,suit=None):
 		# if you don't specify a rank, pick one at random
 		if rank == None:
 			from random import randint
@@ -28,17 +29,17 @@ class card(object):
 			else:
 				# Gave a weird input
 				raise ValueError
-		if suite == None:
+		if suit == None:
 			from random import randint
-			self._suite = randint(0,3)
+			self._suit = randint(0,3)
 		else:
-			if isinstance(suite, int):
+			if isinstance(suit, int):
 				# raises an AssertionError if not within the accepted range
-				assert( 0 <= suite <= 3)
-				self._suite = suite
-			elif isinstance(suite, str):
-				assert( suite in [ key for key in card._suites_reverse ] )
-				self._suite = card._suites_reverse[suite]
+				assert( 0 <= suit <= 3)
+				self._suit = suit
+			elif isinstance(suit, str):
+				assert( suit in [ key for key in card._suits_reverse ] )
+				self._suit = card._suits_reverse[suit]
 			else:
 				# Gave a weird input
 				raise ValueError
@@ -47,21 +48,21 @@ class card(object):
 		'''A getter for the rank of the card '''
 		return self._rank
 
-	def suite(self):
-		'''A getter for the suite of the card '''
-		return self._suite
+	def suit(self):
+		'''A getter for the suit of the card '''
+		return self._suit
 
 	@classmethod
-	def cardCompare(cls, aCard, withSuites = False):
-		if withSuites:
-			return aCard.rank(), aCard.suite()
+	def cardCompare(cls, aCard, withsuits = False):
+		if withsuits:
+			return aCard.rank(), aCard.suit()
 		else:
 			return aCard.rank()
 
-	def _compare(self, other, method, withSuites= False):
+	def _compare(self, other, method, withsuits= False):
 		try:
-			if withSuites:
-				return method(self.rank(), other.rank()) and method(self.suite(), other.suite())
+			if withsuits:
+				return method(self.rank(), other.rank()) and method(self.suit(), other.suit())
 			else:
 				return method(self.rank(), other.rank())
 		except AttributeError:
@@ -115,21 +116,22 @@ class card(object):
 		>>> print(card('Q'))
 Queen of Hearts
 		'''
-		return str(self._ranks[self._rank]) + " of " + str(self._suites[self._suite])
+		return str(self._ranks[self._rank]) + " of " + str(self._suits[self._suit])
 
 	def __repr__(self):
 		'''>>> card('Q')
 Queen of Spades
 		'''
-		return str(self._ranks[self._rank]) + " of " + str(self._suites[self._suite])
+		return str(self._ranks[self._rank]) + " of " + str(self._suits[self._suit])
 
 	@classmethod
-	def suites_name(cls,suite):
-		return card._suites[suite]
+	def suits_name(cls,suit):
+		return card._suits[suit]
 
 	@classmethod
 	def ranks_name(cls,rank):
 		return card._ranks[rank]
+
 
 import unittest
 class TestHand(unittest.TestCase):
@@ -148,12 +150,12 @@ class TestHand(unittest.TestCase):
 		oneS = card('2', 'S')
 		oneC = card('2', 'C')
 		oneD = card('2', 'D')
-		self.assertEqual( sorted( [oneH, oneS], key= lambda x: (x.rank(), x.suite()) ), [oneS, oneH] )
-		self.assertEqual( sorted( [oneH, oneS, oneC, oneD], key= lambda x: card.cardCompare(x,withSuites=True)), [oneC, oneD, oneH, oneS] )
+		self.assertEqual( sorted( [oneH, oneS], key= lambda x: (x.rank(), x.suit()) ), [oneS, oneH] )
+		self.assertEqual( sorted( [oneH, oneS, oneC, oneD], key= lambda x: card.cardCompare(x,withsuits=True)), [oneC, oneD, oneH, oneS] )
 
 		K = card('K')
 		A = card('A')
-		self.assertEqual( sorted( [K, A], key= lambda x: card.cardCompare(x,withSuites=True)), [A,K] )
+		self.assertEqual( sorted( [K, A], key= lambda x: card.cardCompare(x,withsuits=True)), [A,K] )
 		def aceWorthElven(x):
 			if x.rank() == 0:
 				return 11
@@ -161,7 +163,7 @@ class TestHand(unittest.TestCase):
 				return x.rank()
 		two = card('2')
 		# One compare with Ace = 0, one with Ace = 11
-		self.assertEqual( sorted( [A,two], key= lambda x: card.cardCompare(x,withSuites=True)), [A,two] )
+		self.assertEqual( sorted( [A,two], key= lambda x: card.cardCompare(x,withsuits=True)), [A,two] )
 		self.assertEqual( sorted( [A,two], key= aceWorthElven), [two,A] )
 	def test_compare(self):
 		self.assertTrue( card('K') > card('Q'))
@@ -183,10 +185,10 @@ class TestHand(unittest.TestCase):
 	def test_choice(self):
 		# verify random creation
 		c = card()
-		self.assertTrue(c.suite() in [0,1,2,3])
+		self.assertTrue(c.suit() in [0,1,2,3])
 
 	def test_classMethod(self):
-		self.assertEqual(card.suites_name(2),'Hearts')
+		self.assertEqual(card.suits_name(2),'Hearts')
 	
 if __name__ == "__main__":
 	unittest.main()
