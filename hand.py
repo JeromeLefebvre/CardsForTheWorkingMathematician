2561f0ce3a08c0bb93548710dc5540a474e6c636
marioga <mgarcia.math@gmail.com>
2014-02-06 11:31:04 -0800

    Tiny changes and isBlackJack method

--- hand.py
+++ hand.py
@@ -16,12 +16,16 @@ class Hand():
         ''' isBusted() -> bool -- returns if the hand is under 21 points for a choice value of aces '''
         return min(self.value()) <= 21
 
+    def isBlackJack(self):
+        '''isBlackJack() -> bool -- returns whether the hand is a BlackJack '''
+        return (len(self._cards) == 2 and max(self.value())==21)
+
     def value(self):
         ''' value() -> list -- returns a list of one or two possible score for the hand '''
         total = 0
         for card in self._cards:
             total += card.blackjackValue()
-        # if there is one atleast one ace, we get two possible values for the score if it won't go over 21
+        # if there is one at least one ace, we get two possible values for the score if it won't go over 21
         if any(card.isAce() for card in self._cards) and total <= 11:
             total = [total, total+10]
         else:
@@ -37,7 +41,7 @@ class Hand():
             if len(self._cards) == 2 and maxself == 21:
                 maxself = 22
             if len(other._cards) == 2 and maxother == 21:
-                maxself = 22                
+                maxother = 22
             return method(maxself, maxother)
         except AttributeError:
             pass
@@ -93,13 +97,13 @@ class TestHand(unittest.TestCase):
         self.dealerhand = Hand(Card('2'))
         # winning base on the blackjack rule
         self.assertTrue(self.playerhand >  self.dealerhand)
-        
+
         self.playerhand = Hand([Card('K')])
         self.dealerhand = Hand([Card('Q')])
         self.assertTrue(self.playerhand ==  self.dealerhand)
         self.playerhand = Hand(Card('K'))
         self.dealerhand = Hand(Card('K'))
-        self.assertTrue(self.playerhand ==  self.dealerhand)        
+        self.assertTrue(self.playerhand ==  self.dealerhand)
 
 if __name__ == "__main__":
     unittest.main()
