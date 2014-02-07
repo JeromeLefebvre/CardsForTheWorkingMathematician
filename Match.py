13f652fcd72a23299ed71e11fafe856b101c5cda
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-02-06 18:30:08 -0800

    A few changes to match

--- Match.py
+++ Match.py
@@ -5,8 +5,22 @@ from player import NormalPlayer,Dealer
 class Match(object):
 	def __init__(self):
 		self.deck = Deck(8)
-		self.players = [NormalPlayer(),Dealer()]
-		for i in range(0,2):
-			for player in self.players()
-				player.dealCard(self.Deck.pop())
+		self.deck.shuffle()
+		self.dealer = Dealer()
+		self.players = [NormalPlayer()]
 		self.currentPlayer = 0
+		
+		self.dealer.startMatch([self.Deck.pop(), self.Deck.pop()])
+		for player in self.players()
+			player.startMatch([self.Deck.pop(), self.Deck.pop()])
+
+	def __str__(self):
+		return "Players %s, Dealer" % [str(player) for player in self.Players], str(self.dealer)
+
+	def hit(self):
+		assert(self.deck.cardsLeft() > 0)
+		self.players[self.currentPlayer].updateAfterHit(self.Deck.pop())
+
+if __name__ == "__main__":
+	match = Match()
+		
