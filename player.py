94db7d67c0ac99be61e8b11e2db9d1939814717b
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-02-10 20:47:44 -0800

    Starting to improve the coverage of a player

--- player.py
+++ player.py
@@ -12,25 +12,18 @@ from card import Card
 from hand import Hand
 
 class Player(object):
-    def __init__(self, hand=None, money=0, name="Player"):
-        if isinstance(hand,Hand):
-            self._hand = [hand]
-        try:
-            self._money = int(money)
-        except ValueError:
-            raise ValueError("Money needs to be an integer")
-        try:
-            # Name is not a private variable
-            self.name = str(name)
-        except:
-            raise ValueError("Name is not valid, but I'd like to call you John")
+    def __init__(self, money=0, name="Player"):
+        assert(isinstance(money,int)) # money needs to be an integer 
+        self._money = money
+        assert(isinstance(name,str)) # name needs to be a string
+        self.name = name
 
     def hand(self):
-        '''Getter for _hand'''
-        return self._hand
+        ''' hand() -> Hand -- Getter for hand'''
+        return self._hands
 
     def money(self):
-        '''Getter for _money'''
+        ''' money() -> int -- Getter for _money'''
         return self._money
 
     def __str__(self):
@@ -52,8 +45,8 @@ class Player(object):
 
 class NormalPlayer(Player):
     '''This class corresponds to normal players in the table'''
-    def __init__(self, hand=None, money=0, name = "Player"):
-        Player.__init__(self, hand, money, name)
+    def __init__(self, money=0, name="John"):
+        Player.__init__(self, money, name)
 
     def startMatch(self,cards):
         ''' startMatch(list of cards) -> None -- receive a hand of 2 cards at the begining of a match'''
@@ -113,25 +106,12 @@ class NormalPlayer(Player):
         except ValueError:
             print ("Bet is not an integer")
 
-'''
-"Dealer stands on all 17s": In this case, the dealer must continue to take cards ("hit") 
-until his total is 17 or greater. An Ace in the dealer's hand is always counted as 11 if
- possible without the dealer going over 21. For example, (Ace,8) would be 19 and the 
- dealer would stop drawing cards ("stand"). Also, (Ace,6) is 17 and again the dealer will stand. 
- (Ace,5) is only 16, so the dealer would hit. He will continue to draw cards until the hand's value 
- is 17 or more. For example, (Ace,5,7) is only 13 so he hits again. (Ace,5,7,5) makes 18 
- so he would stop ("stand") at that point.
 
-Dealer hits soft 17": Some casinos use this rule variation instead. This rule is identical except 
-for what happens when the dealer has a soft total of 17. Hands such as (Ace,6), (Ace,5,Ace), and 
-(Ace, 2, 4) are all examples of soft 17. The dealer hits these hands, and stands on soft 18 or higher,
- or hard 17 or higher. When this rule is used, the house advantage against the players is slightly increased.
-'''
 class Dealer(Player):
     '''This class corresponds to the dealer. We assign no money to it and interpret
     its money as wins or losses for the house'''
-    def __init__(self, hand=None,name="Malkovich", standOn17=False, soft17=False):
-        Player.__init__(self, hand, 0, "Dealer")
+    def __init__(self,name="Malkovich", standOn17=False, soft17=False):
+        Player.__init__(self, 0, name)
         assert(standOn17 ^ soft17) # Dealer can only follow one rule
         self._standOn17 = standOn17
         self._soft17 = soft17
@@ -153,23 +133,16 @@ class Dealer(Player):
         ''' shoudHit() -> bool -- returns if the dealer wants to hit or not base on the choice of a 17 rule'''
         if self._standOn17:
             return self._hand.bestValue() < 17
-        elif self._soft17:
-            # Soft 17
+        else: # soft17
             if self._hand.bestValue() == 17 and len(self._hand.value()) == 2:
                 return True
-            # Hard 17 or soft >= 18
-            elif self._hand.bestValue() >= 17:
+            elif self._hand.bestValue() >= 17:  # Hard 17 or soft >= 18
                 return False
             else:
                 return True
-        else:
-            raise ValueError("Dealer is not setup well")
 
 import unittest
 class TestPlayer(unittest.TestCase):
-    def setUp(self):
-        pass
-
     def test_basicPlayer(self):
         pass
 
@@ -181,6 +154,20 @@ class TestPlayer(unittest.TestCase):
         john.collectBet(20)
         self.assertTrue(john.canBet(25))
 
+    '''
+    "Dealer stands on all 17s": In this case, the dealer must continue to take cards ("hit") 
+    until his total is 17 or greater. An Ace in the dealer's hand is always counted as 11 if
+     possible without the dealer going over 21. For example, (Ace,8) would be 19 and the 
+     dealer would stop drawing cards ("stand"). Also, (Ace,6) is 17 and again the dealer will stand. 
+     (Ace,5) is only 16, so the dealer would hit. He will continue to draw cards until the hand's value 
+     is 17 or more. For example, (Ace,5,7) is only 13 so he hits again. (Ace,5,7,5) makes 18 
+     so he would stop ("stand") at that point.
+
+    Dealer hits soft 17": Some casinos use this rule variation instead. This rule is identical except 
+    for what happens when the dealer has a soft total of 17. Hands such as (Ace,6), (Ace,5,Ace), and 
+    (Ace, 2, 4) are all examples of soft 17. The dealer hits these hands, and stands on soft 18 or higher,
+     or hard 17 or higher. When this rule is used, the house advantage against the players is slightly increased.
+    '''
     def test_dealer(self):
         aDealer = Dealer(standOn17=True)
         aDealer.startMatch([Card('K'), Card('7')]) # hit 17
@@ -209,18 +196,3 @@ class TestPlayer(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
-    myplayer = NormalPlayer(Hand([Card(),Card()]),5)
-    print(myplayer)
-    print(myplayer.hand().value())
-    myplayer.updateAfterHit(Card())
-    print(myplayer)
-    print(myplayer.hand().value())
-    myplayer.updateAfterDouble(Card(),5)
-    print(myplayer)
-    print(myplayer.hand().value())
-    myplayer.updateAfterDouble(Card(),5)
-    print(myplayer)
-    print(myplayer.hand().value())
-
-
